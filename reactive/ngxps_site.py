2ba6451a2e3a012f9ae6cc751daa8c0a817176a4
Rodolfo E. Edelmann <redelmann@machinalis.com>
2016-07-10 00:16:09 -0300

    add server_name, remove log

--- reactive/ngxps_site.py
+++ reactive/ngxps_site.py
@@ -1,5 +1,7 @@
 import os
 
+from shutil import rmtree
+
 from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler
@@ -15,17 +17,17 @@ def install_site(engine):
     if not data_changed('config', config):
         return
 
-    hookenv.log(config)
-    hookenv.log(config['EnableFilters'])
-
     site_path = os.path.join('/usr/local/nginx/html', hookenv.service_name())
+    rmtree(site_path)
 
     handler = ArchiveUrlFetchHandler()
     handler.install(config['default_site'], site_path)
 
     root = os.path.join(site_path, config['root'])
 
-    engine.configure(root=root, listen=config['listen'],
+    engine.configure(root=root,
+                     listen=config['listen'],
+                     server_name=config['server_name'],
                      RewriteLevel=config['RewriteLevel'],
                      EnableFilters=config['EnableFilters'],
                      LearningMode=config['LearningMode'])
